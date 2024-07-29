05c4a47f60d4bb938995fce5a339e6b8949ac185
ag142 <aelligupta142@gmail.com>
2024-07-29 15:26:49 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ if(process.env.NODE_ENV != "production"){
 const express  = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index')
+
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({limit: '10mb',extended:false}))
 
 
 const mongoose = require("mongoose")
@@ -24,5 +31,7 @@ db.once('open',()=> console.log('Connected to mongoose'))
 
 
 app.use('/',indexRouter)
+
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
